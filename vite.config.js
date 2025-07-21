03db19a05df7485e998703c1126730353617b913
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2025-07-21 23:10:22 +0530

    Add some dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  theme: {
+    extends: {
+      fontSize: {
+        'course-deatails-heading-small':['26px','36px'],
+        'course-deatails-heading-large':['36px','44px'],
+        'home-heading-small':['24px','34px'],
+        'home-heading-large':['48px','56px'],
+        'default':['15px','21px']
+      }
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
