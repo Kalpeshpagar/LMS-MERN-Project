19dc2c49275820bbae5fef06602acf1485436fa1
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2025-07-23 19:09:43 +0530

    Add Courses in CourseCard section

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,12 +1,25 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
+    const [allCourses,setAllCourses] =useState([])
 
+    // fetch all course
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(() => {
+    fetchAllCourses()
+},[])
     const value = {
-
+currency,allCourses,navigate
     }
 
     return (
