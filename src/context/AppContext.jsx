c1d2dca130157dec5a88ece27aec41b4f6a895e3
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2025-07-24 23:13:41 +0530

    Add Testimonial Cards

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -9,17 +9,31 @@ export const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
     const [allCourses,setAllCourses] =useState([])
+    const [isEducator,setIsEducator] =useState(true)
 
     // fetch all course
     const fetchAllCourses = async () => {
         setAllCourses(dummyCourses)
     }
 
+    // function to calculate average rating of course
+    const calculateRating = (course) => {
+        if (course.courseRatings.length === 0) {
+            return 0;
+        }
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
+
     useEffect(() => {
     fetchAllCourses()
 },[])
-    const value = {
-currency,allCourses,navigate
+    const value = { // we access it any other component
+        currency, allCourses, navigate, calculateRating,
+        isEducator,setIsEducator
     }
 
     return (
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
         'home-heading-small':['24px','34px'],
         'home-heading-large':['48px','56px'],
         'default':['15px','21px']
+      },
+      gridTemplateColumns: {
+        'auto':'repeat(auto-fit, minmax(200px, 1fr))'
       }
     },
   },
